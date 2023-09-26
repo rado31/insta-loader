2f75638ff61ab245b42d7dde92ee54de9f802ad4
Dovran Rahymov <dova.jarvis@gmail.com>
2023-09-26 21:16:06 +0500

    updated: read from file

--- index.py
+++ index.py
@@ -3,5 +3,10 @@ from instaloader import Instaloader, Post
 
 loader = Instaloader()
 
-post = Post.from_shortcode(loader.context, 'CwnrCzMBL1y')
-loader.download_post(post, 'CwnrCzMBL1y')
+file = open('./urls.txt', 'r')
+urls = file.readlines()
+
+for url in urls:
+    video_id = url.split('/')[4]
+    post = Post.from_shortcode(loader.context, video_id)
+    loader.download_post(post, video_id)
